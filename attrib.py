f21ec79790d0f35e2ba69ccb246406413d0665be
wzh99 <wzh1999_frog@126.com>
2020-12-30 20:27:19 +0800

    Add attribute for ops.

---
+++ attrib.py
@@ -0,0 +1,49 @@
+from typing import Union
+from enum import Enum
+
+
+class AttribExpr:
+    """
+    AST for attribute expression.
+    """
+
+    def __add__(self, other):
+        return BinaryExpr(BinaryOp.ADD, self, other)
+
+    def __sub__(self, other):
+        return BinaryExpr(BinaryOp.SUB, self, other)
+
+
+class ConstAttrib(AttribExpr):
+    """
+    A compile-time constant attribute value.
+    """
+
+    def __init__(self, value: Union[bool, int, tuple, list, str]):
+        self.value = value
+
+
+class GetAttrib(AttribExpr):
+    """
+    Access attribute from a graph node.
+    """
+
+    def __init__(self, node, name: str):
+        self.node = node
+        self.name = name
+
+
+class BinaryOp(Enum):
+    ADD = '+'
+    SUB = '-'
+
+
+class BinaryExpr(AttribExpr):
+    """
+    Binary expression of attribute values.
+    """
+
+    def __init__(self, op: BinaryOp, lhs: AttribExpr, rhs: AttribExpr):
+        self.op = op
+        self.lhs = lhs
+        self.rhs = rhs
--- graph.py
+++ graph.py
@@ -1,8 +1,13 @@
 import numpy as np
-from typing import Union
+from typing import Union, Dict, Any, Generic
+from attrib import GetAttrib
 
 
 class Node:
+    """
+    Base class for all graph nodes. This class cannot be instantiated.
+    """
+
     def __neg__(self):
         return Call('negative', self)
 
@@ -20,20 +25,51 @@ class Node:
 
 
 class Wildcard(Node):
+    """
+    A wildcard node matches all nodes in graph. Target graph cannot contain wildcard nodes not
+    defined in source graph.
+    """
     pass
 
 
 class Var(Node):
+    """
+    A variable node matches input tensor of the model. Target graph cannot contain variable nodes
+    not defined in source graph.
+    """
     pass
 
 
-class Constant(Node):
-    def __init__(self, value: Union[int, float, list, np.ndarray]):
+class Const(Node):
+    """
+    A constant nodes stores constants in graph.
+    """
+
+    def __init__(self, value: Union[int, float, list, np.ndarray, None]):
+        """
+        Constructor
+        :param value: In source graph, if the value is provided, it only matches nodes with the
+        same value. Otherwise, any constant node will match. If the node also appears in target
+        graph, the value will be copied to new graph. New constant nodes can also be created in
+        target graph.
+        """
+        if value is None:
+            return
         if isinstance(value, (int, float, list)):
             value = np.array(value)
         if not isinstance(value, np.ndarray):
             raise RuntimeError('Not a constant')
         self.value = value
+
+
+class Call(Node):
+    def __init__(self, op: str, *args: Node, **attrib):
+        self.op = op
+        self.args = args
+        self.attrib = attrib
+
+    def __getattr__(self, name: str):
+        return GetAttrib(self, name)
 
 
 class Tuple(Node):
@@ -50,7 +86,45 @@ class GetItem(Node):
         self.index = index
 
 
-class Call(Node):
-    def __init__(self, op: str, *args):
-        self.op = op
-        self.args = args
+class NodeVisitor:
+    visited: Dict[Node, Any] = dict()
+
+    def visit(self, node: Node):
+        if self.visited.__contains__(node):
+            return self.visited[node]
+        if isinstance(node, Wildcard):
+            ret = self.visit_wildcard(node)
+        elif isinstance(node, Var):
+            ret = self.visit_var(node)
+        elif isinstance(node, Const):
+            ret = self.visit_const(node)
+        elif isinstance(node, Call):
+            ret = self.visit_call(node)
+        elif isinstance(node, Tuple):
+            ret = self.visit_tuple(node)
+        elif isinstance(node, GetItem):
+            ret = self.visit_getitem(node)
+        else:
+            raise RuntimeError('Unhandled node case')
+        self.visited[node] = ret
+        return ret
+
+    def visit_wildcard(self, wildcard: Wildcard) -> Any:
+        pass
+
+    def visit_var(self, var: Var) -> Any:
+        pass
+
+    def visit_const(self, const: Const) -> Any:
+        pass
+
+    def visit_call(self, call: Call) -> Any:
+        for arg in call.args:
+            self.visit(arg)
+
+    def visit_tuple(self, tp: Tuple) -> Any:
+        for f in tp.fields:
+            self.visit(f)
+
+    def visit_getitem(self, getitem: GetItem) -> Any:
+        self.visit(getitem.tup)
