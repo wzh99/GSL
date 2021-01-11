c0930c7bc12ddbc73e4e3456551b18e8c7826ac6
wzh99 <wzh1999_frog@126.com>
2021-01-11 16:50:38 +0800

    Add sequential substitution test case.

--- rule.py
+++ rule.py
@@ -16,6 +16,21 @@ def trans_trans():
     return Substitution(y1, y2)
 
 
+def split_concat():
+    # Input
+    x = Wildcard()
+
+    # Source pattern: concat(split(x, axis=a), axis=a)
+    split = Call('split', x, indices_or_sections=2)
+    y1 = Call('concatenate', Tuple(split[0], split[1]), axis=split.axis)
+
+    # Target pattern: x
+    y2 = x
+
+    # Build substitution
+    return Substitution(y1, y2)
+
+
 def bias_add_add():
     # Input
     x1 = Wildcard()
@@ -35,16 +50,77 @@ def bias_add_add():
     return Substitution(y1, y2)
 
 
-def split_concat():
+def diamond_conv_add():
     # Input
     x = Wildcard()
+    w1 = Var()
+    w2 = Var(shape=w1.shape)
 
-    # Source pattern: concat(split(x, axis=a), axis=a)
-    split = Call('split', x, indices_or_sections=2)
-    y1 = Call('concatenate', Tuple(split[0], split[1]), axis=split.axis)
+    # Source pattern: conv2d(x, w1) + conv2d(x, w2)
+    conv1 = Call('nn.conv2d', x, w1)
+    conv2 = Call('nn.conv2d', x, w2, strides=conv1.strides, padding=conv1.padding,
+                 dilation=conv1.dilation, groups=conv1.groups)
+    y1 = conv1 + conv2
 
-    # Target pattern: x
-    y2 = x
+    # Target pattern: conv2d(x, w1 + w2)
+    y2 = Call('nn.conv2d', x, w1 + w2, strides=conv1.strides, padding=conv1.padding,
+              dilation=conv1.dilation, groups=conv1.groups)
+
+    # Build substitution
+    return Substitution(y1, y2)
+
+
+def two_conv_add():
+    # Input
+    x1 = Wildcard()
+    x2 = Wildcard()
+    w1 = Var()
+    w2 = Var(shape=w1.shape)
+
+    # Source pattern: conv2d(x1, w1) + conv2d(x2, w2)
+    conv1 = Call('nn.conv2d', x1, w1)
+    conv2 = Call('nn.conv2d', x2, w2, strides=conv1.strides, padding=conv1.padding,
+                 dilation=conv1.dilation, groups=conv1.groups)
+    y1 = conv1 + conv2
+
+    # Target pattern: conv2d(concat(x1, x2), concat(w1, w2))
+    x_concat = Call('concatenate', Tuple(x1, x2), axis=1)
+    w_concat = Call('concatenate', Tuple(w1, w2), axis=1)
+    y2 = Call('nn.conv2d', x_concat, w_concat, strides=conv1.strides, padding=conv1.padding,
+              dilation=conv1.dilation, groups=conv1.groups)
+
+    # Build substitution
+    return Substitution(y1, y2)
+
+
+def conv_batch_norm():
+    # Input
+    x = Wildcard()
+    w = Var()
+    gamma = Var()
+    beta = Var()
+    moving_mean = Var()
+    moving_var = Var()
+
+    # Source pattern
+    conv = Call('nn.conv2d', x, w)
+    bn = Call('nn.batch_norm', conv, gamma, beta, moving_mean, moving_var)
+    y1 = bn[0]
+    # y1.visualize('conv_bn_pat')
+
+    # Target pattern
+    k = gamma / Call('sqrt', moving_var + bn.epsilon)
+    out_chan = gamma.shape[0]
+    zeros = Call('zeros', shape=(out_chan, out_chan), dtype=w.dtype)
+    diag = Call('expand_dims', Call('matrix_set_diag', zeros, k), axis=0)
+    conv_w = Call('reshape', w, newshape=(1, w.shape[0], -1))
+    matmul = Call('nn.batch_matmul', diag, Call('transpose', conv_w, axes=[0, 2, 1]))
+    fused_w = Call('reshape', matmul, newshape=w.shape)
+    new_conv = Call('nn.conv2d', x, fused_w, strides=conv.strides, padding=conv.padding,
+                    dilation=conv.dilation, groups=conv.groups)
+    bias = beta - moving_mean * k
+    y2 = Call('nn.bias_add', new_conv, bias)
+    # y2.visualize('conv_bias_add_pat')
 
     # Build substitution
     return Substitution(y1, y2)
@@ -120,36 +196,3 @@ def parallel_conv_expand_kernels():
 
     # Build substitution
     return Substitution([conv1, conv2], [split[0], split[1]])
-
-
-def conv_batch_norm():
-    # Input
-    x = Wildcard()
-    w = Var()
-    gamma = Var()
-    beta = Var()
-    moving_mean = Var()
-    moving_var = Var()
-
-    # Source pattern
-    conv = Call('nn.conv2d', x, w)
-    bn = Call('nn.batch_norm', conv, gamma, beta, moving_mean, moving_var)
-    y1 = bn[0]
-    # y1.visualize('conv_bn_pat')
-
-    # Target pattern
-    k = gamma / Call('sqrt', moving_var + bn.epsilon)
-    out_chan = gamma.shape[0]
-    zeros = Call('zeros', shape=(out_chan, out_chan), dtype=w.dtype)
-    diag = Call('expand_dims', Call('matrix_set_diag', zeros, k), axis=0)
-    conv_w = Call('reshape', w, newshape=(1, w.shape[0], -1))
-    matmul = Call('nn.batch_matmul', diag, Call('transpose', conv_w, axes=[0, 2, 1]))
-    fused_w = Call('reshape', matmul, newshape=w.shape)
-    new_conv = Call('nn.conv2d', x, fused_w, strides=conv.strides, padding=conv.padding,
-                    dilation=conv.dilation, groups=conv.groups)
-    bias = beta - moving_mean * k
-    y2 = Call('nn.bias_add', new_conv, bias)
-    # y2.visualize('conv_bias_add_pat')
-
-    # Build substitution
-    return Substitution(y1, y2)
--- test.py
+++ test.py
@@ -23,24 +23,6 @@ class RuleTest(unittest.TestCase):
         print(wl.mod)
         self.assertTrue(True)
 
-    def test_bias_add_add(self):
-        print('BiasAdd-Add')
-
-        # Source graph
-        x1 = relay.var('x1', shape=(4, 3, 32, 32))
-        x2 = relay.var('x2', shape=(4, 3, 32, 32))
-        b1 = relay.var('b1', shape=(3,))
-        b2 = relay.var('b2', shape=(3,))
-        y = relay.nn.bias_add(x1, b1) + relay.nn.bias_add(x2, b2)
-        wl = Workload.from_expr(y, {'x1', 'x2'})
-        print(wl.mod)
-
-        # Apply substitution
-        subst = rule.bias_add_add()
-        wl = subst(wl)
-        print(wl.mod)
-        self.assertTrue(True)
-
     def test_split_concat(self):
         print('Split-Concat')
 
@@ -54,6 +36,84 @@ class RuleTest(unittest.TestCase):
         # Apply substitution
         subst = rule.split_concat()
         wl = subst(wl)
+        print(wl.mod)
+        self.assertTrue(True)
+
+    def test_bias_add_add(self):
+        print('BiasAdd-Add')
+
+        # Source graph
+        x1 = relay.var('x1', shape=(2, 3, 4, 4))
+        x2 = relay.var('x2', shape=(2, 3, 4, 4))
+        b1 = relay.var('b1', shape=(3,))
+        b2 = relay.var('b2', shape=(3,))
+        y = relay.nn.bias_add(x1, b1) + relay.nn.bias_add(x2, b2)
+        wl = Workload.from_expr(y, {'x1', 'x2'})
+        print(wl.mod)
+
+        # Apply substitution
+        subst = rule.bias_add_add()
+        wl = subst(wl)
+        print(wl.mod)
+        self.assertTrue(True)
+
+    def test_diamond_conv_add(self):
+        print('Diamond Conv-Add')
+
+        # Source graph
+        x = relay.var('x', shape=(2, 2, 4, 4))
+        w1 = relay.var('w1', shape=(4, 2, 1, 1))
+        w2 = relay.var('w2', shape=(4, 2, 1, 1))
+        conv1 = relay.nn.conv2d(x, w1)
+        conv2 = relay.nn.conv2d(x, w2)
+        y = conv1 + conv2
+        wl = Workload.from_expr(y, {'x'})
+        print(wl.mod)
+
+        # Apply substitution
+        subst = rule.diamond_conv_add()
+        wl = subst(wl)
+        print(wl.mod)
+        self.assertTrue(True)
+
+    def test_two_conv_add(self):
+        print('Two Conv-Add')
+
+        # Source graph
+        x1 = relay.var('x1', shape=(2, 2, 4, 4))
+        w1 = relay.var('w1', shape=(4, 2, 1, 1))
+        conv1 = relay.nn.conv2d(x1, w1)
+        x2 = relay.var('x2', shape=(2, 2, 4, 4))
+        w2 = relay.var('w2', shape=(4, 2, 1, 1))
+        conv2 = relay.nn.conv2d(x2, w2)
+        y = conv1 + conv2
+        wl = Workload.from_expr(y, {'x1', 'x2'})
+        print(wl.mod)
+
+        # Apply substitution
+        subst = rule.two_conv_add()
+        wl = subst(wl)
+        print(wl.mod)
+        self.assertTrue(True)
+
+    def test_conv_batch_norm(self):
+        print('Conv-BatchNorm')
+
+        # Source graph
+        x = relay.var('x', shape=(2, 2, 4, 4))
+        w = relay.var('w', shape=(2, 2, 3, 3))
+        gamma = relay.var('gamma', shape=(2,))
+        beta = relay.var('beta', shape=(2,))
+        moving_mean = relay.var('moving_mean', shape=(2,))
+        moving_var = relay.var('moving_var', shape=(2,))
+        y = relay.nn.conv2d(x, w, padding=(1, 1, 1, 1))
+        y = relay.nn.batch_norm(y, gamma, beta, moving_mean, moving_var)[0]
+        wl = Workload.from_expr(y, {'x'}, name='conv_bn')
+        print(wl.mod)
+
+        # Apply substitution
+        subst = rule.conv_batch_norm()
+        wl = subst(wl, new_name='conv_bias_add')
         print(wl.mod)
         self.assertTrue(True)
 
@@ -116,24 +176,45 @@ class RuleTest(unittest.TestCase):
         print(wl.mod)
         self.assertTrue(True)
 
-    def test_conv_batch_norm(self):
-        print('Conv-BatchNorm')
+    def test_sequential_subst(self):
+        print('Sequential Subst. on a Simplified NASNet Block')
 
         # Source graph
-        x = relay.var('x', shape=(2, 2, 4, 4))
-        w = relay.var('w', shape=(2, 2, 3, 3))
-        gamma = relay.var('gamma', shape=(2,))
-        beta = relay.var('beta', shape=(2,))
-        moving_mean = relay.var('moving_mean', shape=(2,))
-        moving_var = relay.var('moving_var', shape=(2,))
-        y = relay.nn.conv2d(x, w, padding=(1, 1, 1, 1))
-        y = relay.nn.batch_norm(y, gamma, beta, moving_mean, moving_var)[0]
-        wl = Workload.from_expr(y, {'x'}, name='conv_bn')
+        x = relay.var('x', shape=(2, 32, 32, 32))
+        relu1 = relay.nn.relu(x)
+        dep_w1 = relay.var('dep_w1', shape=(32, 1, 3, 3))
+        dep_conv1 = relay.nn.conv2d(relu1, dep_w1, padding=(1, 1, 1, 1), groups=32)
+        pt_w1 = relay.var('pt_w1', shape=(32, 32, 1, 1))
+        pt_conv1 = relay.nn.conv2d(dep_conv1, pt_w1)
+        g1 = relay.var('g1', shape=(32,))
+        b1 = relay.var('b1', shape=(32,))
+        m1 = relay.var('m1', shape=(32,))
+        v1 = relay.var('v1', shape=(32,))
+        bn1 = relay.nn.batch_norm(pt_conv1, g1, b1, m1, v1)[0]
+        relu2 = relay.nn.relu(x)
+        w2 = relay.var('dep_w2', shape=(32, 1, 5, 5))
+        dep_conv2 = relay.nn.conv2d(relu2, w2, padding=(2, 2, 2, 2), groups=32)
+        pt_w2 = relay.var('pt_w2', shape=(32, 32, 1, 1))
+        pt_conv2 = relay.nn.conv2d(dep_conv2, pt_w2)
+        g2 = relay.var('g2', shape=(32,))
+        b2 = relay.var('b2', shape=(32,))
+        m2 = relay.var('m2', shape=(32,))
+        v2 = relay.var('v2', shape=(32,))
+        bn2 = relay.nn.batch_norm(pt_conv2, g2, b2, m2, v2)[0]
+        y = bn1 + bn2
+        wl = Workload.from_expr(y, {'x'})
         print(wl.mod)
 
-        # Apply substitution
-        subst = rule.conv_batch_norm()
-        wl = subst(wl, new_name='conv_bias_add')
+        # Apply substitutions
+        for subst in [
+            rule.merge_relu(),
+            rule.parallel_conv_expand_kernels(),
+            rule.conv_batch_norm(),
+            rule.bias_add_add(),
+            rule.two_conv_add(),
+            rule.split_concat()
+        ]:
+            wl = subst(wl, fast_mode=True)
         print(wl.mod)
         self.assertTrue(True)
 
@@ -161,6 +242,7 @@ class ModelTest(unittest.TestCase):
         import model
         net = model.nasnet.get_model(1)
         wl = Workload.from_keras(net, {'input_1': model.batch_shape_nchw})
+        print(wl.mod)
         # wl.visualize()
 
         # Apply substitution
@@ -177,12 +259,15 @@ class ModelTest(unittest.TestCase):
 if __name__ == '__main__':
     suite = unittest.TestSuite(tests=[
         # RuleTest('test_trans_trans'),
-        # RuleTest('test_bias_add_add'),
         # RuleTest('test_split_concat'),
+        # RuleTest('test_bias_add_add'),
+        # RuleTest('test_diamond_conv_add'),
+        # RuleTest('test_two_conv_add'),
+        # RuleTest('test_conv_batch_norm'),
         # RuleTest('test_merge_relu'),
         # RuleTest('test_parallel_conv'),
         # RuleTest('test_parallel_conv_expand_kernels'),
-        # RuleTest('test_conv_batch_norm'),
+        # RuleTest('test_sequential_subst'),
         # ModelTest('test_resnet'),
         # ModelTest('test_nasnet'),
     ])
