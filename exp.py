278bc7a57c4d325407a0b60e6e1614dfe09a1581
wzh99 <wzh1999_frog@126.com>
2021-04-06 22:49:52 +0800

    Write framework of experiments.

---
+++ exp.py
@@ -0,0 +1,93 @@
+from typing import Tuple, Set, Dict, Optional
+
+from tvm import relay, transform, ir
+from tvm.relay import dataflow_pattern as dfp
+
+from gsl import *
+
+
+class SubstTest(dfp.DFPatternCallback):
+    def create_expr(self) -> Tuple[relay.Expr, Set[str]]:
+        pass
+
+    def get_pass(self) -> transform.Pass:
+        pass
+
+    def define_gsl(self) -> Optional[Subst]:
+        pass
+
+    def rewrite_dfp(self, node_map: Dict[dfp.DFPattern, relay.Expr]) -> relay.Expr:
+        pass
+
+    def callback(self, pre: relay.Expr, post: relay.Expr, node_map: ir.Map) -> relay.Expr:
+        return self.rewrite_dfp(dict([(p, l[0]) for p, l in node_map.items()]))
+
+    def run(self):
+        # Create workload
+        body, input_names = self.create_expr()
+        wl = Workload.from_expr(body, input_names)
+        print('Original module:')
+        print(wl.mod)
+
+        # Apply built-in pass
+        pass_mod = self.get_pass()(wl.mod)
+        print('After built-in pass:')
+        print(pass_mod)
+
+        # Use Relay DFP for substitution
+        if self.pattern is not None:
+            dfp_body = self.rewrite(body)
+            dfp_mod = ir.IRModule(functions={
+                'main': relay.Function(relay.analysis.free_vars(dfp_body),
+                                       dfp_body)
+            })
+            print('After DFP rewrite:')
+            print(dfp_mod)
+
+        # Use GSL for substitution
+        rule = self.define_gsl()
+        if rule is not None:
+            gsl_wl = rule(wl, fast_mode=True, fold_params=False)
+            print('After GSL substitution:')
+            print(gsl_wl.mod)
+
+
+class SimplifyBatchNorm(SubstTest):
+    def create_expr(self) -> Tuple[relay.Expr, Set[str]]:
+        x = relay.var('x', shape=(2, 2, 4, 4))
+        gamma = relay.var('gamma', shape=(2,))
+        beta = relay.var('beta', shape=(2,))
+        mean = relay.var('mean', shape=(2,))
+        var = relay.var('var', shape=(2,))
+        bn = relay.nn.batch_norm(x, gamma, beta, mean, var)
+        return bn[0], {'x'}
+
+    def get_pass(self) -> transform.Pass:
+        return relay.transform.SimplifyInference()
+
+    def define_gsl(self) -> Optional[Subst]:
+        # Input
+        x = pat.Wildcard()
+        gamma = pat.Variable()
+        beta = pat.Variable()
+        mean = pat.Variable()
+        var = pat.Variable()
+
+        # Source pattern: batch_norm(x, gamma, beta, mean, var)
+        bn = op.BatchNorm(x, gamma, beta, mean, var, axis=1, scale=True, center=True)
+        y1 = bn[0]
+
+        # Target pattern: k = gamma / sqrt(var + epsilon), x * k + beta - mean * k
+        k = gamma / op.Sqrt(var + bn.epsilon)
+        bias = beta - mean * k
+        y2 = op.BiasAdd(x * op.ExpandDims(k, axis=1, num_newaxis=2), bias)
+
+        # Build substitution
+        return Subst(y1, y2)
+
+
+if __name__ == '__main__':
+    for cls in [
+        SimplifyBatchNorm,
+    ]:
+        cls().run()
